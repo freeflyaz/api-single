f07b1bae797cc91167f4fb5b41a6dd9adb1224f6
Gabe Mata <gabemata@gmail.com>
2024-02-17 19:51:06 +0100

    feat: card demo tailwind

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,12 @@
-import './App.css'
+import React from 'react';
+import Card from './Card'; // Adjust the import path to where your Card component is located
 
-function App() {
-
+const App = () => {
   return (
-<h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <div className="App">
+      <Card />
+    </div>
+  );
+};
 
-export default App
+export default App;
