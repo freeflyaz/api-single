2679a07f220fd1363fd58cf72d16662af2b56ba6
Gabe Mata <gabemata@gmail.com>
2024-02-17 20:59:48 +0100

    featL chackra

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import Card from './Card'; // Adjust the import path to where your Card component is located
+import { ChakraProvider, Box, Text, Button, VStack } from '@chakra-ui/react';
 
 const App = () => {
   return (
+    <>
     <div className="App">
       <Card />
     </div>
+    <ChakraProvider>
+    <Box textAlign="center" fontSize="xl" p={5}>
+      <VStack spacing={8}>
+        <Text>Welcome to Chakra UI</Text>
+        <Button colorScheme="teal" size="lg">
+          Click me
+        </Button>
+      </VStack>
+    </Box>
+  </ChakraProvider>
+  </>
   );
 };
 
