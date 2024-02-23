9c07bc949aceef5e92b0b33c822e846b1207a5d6
Gabe Mata <gabemata@gmail.com>
2024-02-23 21:10:50 +0100

    feat: checkAnswerHandler Not Working Yet

--- vite-project/src/components/quizMode.jsx
+++ vite-project/src/components/quizMode.jsx
@@ -1,47 +1,55 @@
 import { Button } from '@chakra-ui/react';
+import { useState } from 'react';
 
 const QuizMode = ({ eventList }) => {
-  const answerButtonStyle = (answerKey) => {
-    return 'mt-4 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-full text-left';
-  };
+    const [buttonAStyle, setButtonAStyle] = useState('initialStyle');
+//   const answerButtonStyle = (answerKey) => {
+//     return 'mt-4 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-full text-left';
+//   };
+
+  const initialStyle = 'mt-4 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-full text-left';
+  const newStyle = 'mt-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded w-full text-left';
+
   function checkAnswerHandler(e) {
     console.log(e.currentTarget.id);
-    if (e.currentTarget.id === eventList.correct)
+    if (e.currentTarget.id === eventList.correct) {
         console.log('Correct : ', eventList.correct);
-    else
+        setButtonAStyle('newStyle');
+    } else {
         console.log('wrong');
-  }
+    }
+}
   return (
     <>
       <div>{eventList.name}</div>
-      <Button
+      <button
         id="A"
-        className={answerButtonStyle('answerA')}
+        className={buttonAStyle === 'initialStyle' ? initialStyle : newStyle}
         onClick={checkAnswerHandler}
       >
         {eventList.answerA}
-      </Button>
-      <Button
+      </button>
+      <button
         id="B"
-        className={answerButtonStyle('answerB')}
+        className={buttonAStyle === 'initialStyle' ? initialStyle : newStyle}
         onClick={checkAnswerHandler}
       >
         {eventList.answerB}
-      </Button>
-      <Button
+      </button>
+      <button
         id="C"
-        className={answerButtonStyle('answerC')}
+        className={buttonAStyle === 'initialStyle' ? initialStyle : newStyle}
         onClick={checkAnswerHandler}
       >
         {eventList.answerC}
-      </Button>
-      <Button
+      </button>
+      <button
         id="D"
-        className={answerButtonStyle('answerD')}
+        className={buttonAStyle === 'initialStyle' ? initialStyle : newStyle}
         onClick={checkAnswerHandler}
       >
         {eventList.answerD}
-      </Button>
+      </button>
     </>
   );
 };
