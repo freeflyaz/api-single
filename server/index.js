90242a9e281c5b9787e379f5f738718690070f6c
Gabe Mata <gabemata@gmail.com>
2024-02-25 15:08:04 +0100

    feat  rails port issues : https://docs.railway.app/guides/fixing-common-errors + db link was wrong

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ const  cors = require('cors');
 const router = require('./router');
 const  app = express();
 
-const port = 3000;
+//const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -11,6 +12,6 @@ app.use(express.json());
 
 app.use(router);
   
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Example app listening on port ${port}`);
 });
