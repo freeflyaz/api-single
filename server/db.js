f9b6be6707ccd2b6975f4467b01645486512b456
Gabe Mata <gabemata@gmail.com>
2024-02-25 14:44:05 +0100

    feat: online mongo

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
   async function main () {
-   await mongoose.connect('mongodb://localhost:27017/SingleDB');
+   await mongoose.connect('mongodb+srv://dbUser:newDbPass@cluster0.knm2nks.mongodb.net/');
   //await mongoose.connect('mongodb+srv://dbUser:newDbPass@cluster0.knm2nks.mongodb.net/SingleDB');
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
